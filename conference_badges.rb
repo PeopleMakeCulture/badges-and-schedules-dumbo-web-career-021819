6b80dd911f220fdd279f5324514f8026accdf6bb
Jing Cao <jingcao.yale@gmail.com>
2019-02-03 22:17:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,28 +3,10 @@ def badge_maker (name)
   return "Hello, my name is #{name}."
 end
 
-# original
-def batch_badge_creator1(names)
-  badges = []
-    names.each do |name|
-      badges.push(badge_maker(name))
-    end
-  return badges
-end
-
 #optimized
 #array.map returns new array with block applied to every element
 def batch_badge_creator(names)
   names.map {|n| badge_maker(n)}
-end
-
-#original
-def assign_rooms1(names)
-  rooms = []
-  names.each_with_index do |name, i|
-    rooms.push("Hello, #{name}! You'll be assigned to room #{i+1}!")
-  end
-  rooms
 end
 
 #optimized
@@ -36,4 +18,22 @@ end
 def printer(names)
   batch_badge_creator(names).each {|n| puts n}
   assign_rooms(names).each {|n| puts n}
+end
+
+# original
+def batch_badge_creator1(names)
+  badges = []
+    names.each do |name|
+      badges.push(badge_maker(name))
+    end
+  return badges
+end
+
+#original
+def assign_rooms1(names)
+  rooms = []
+  names.each_with_index do |name, i|
+    rooms.push("Hello, #{name}! You'll be assigned to room #{i+1}!")
+  end
+  rooms
 end
